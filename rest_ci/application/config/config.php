dc4b4421dbc9deb7c7be70b3dfbb49df7add3882
Zainal Abidin <zainalabidin170197@gmail.com>
2020-10-08 20:06:55 +0700

    coba ini zainal

--- rest_ci/application/config/config.php
+++ rest_ci/application/config/config.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
-| Base Site URL cobacoba
+| Base Site URL ini saya cobak by zainal
 |--------------------------------------------------------------------------
 |
 | URL to your CodeIgniter root. Typically this will be your base URL,
