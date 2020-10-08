d55134688d165772ce5e6f7b59c27ac23f230cf5
Yoga Aditya <50347730+yogaaditya4x@users.noreply.github.com>
2020-10-08 21:19:32 +0700

    add coment //
    coba ubah - commit

--- rest_ci/application/config/config.php
+++ rest_ci/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ''; //ini saya coba yoga
+$config['base_url'] = ''; //ini saya coba ubah lagi (yoga)
 
 /*
 |--------------------------------------------------------------------------
