756b48757002e1a6e5af85e53ae079c9ff1745ce
egyraka <72249053+egyraka@users.noreply.github.com>
2020-10-08 22:23:30 +0700

    Update config.php
    Oh mayan ngertilah. thanks to zainal

--- rest_ci/application/config/config.php
+++ rest_ci/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = ''; //ini saya coba ubah lagi (yoga)
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'index.php'; //ini nulis terserah dimana? (egy)
 
 /*
 |--------------------------------------------------------------------------
